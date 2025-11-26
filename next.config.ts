fa5a3e8b73bb47a6d6e083d70639c594f9a81560
Mulkesh-sharma <mulkeshsharma2004@gmail.com>
2025-11-26 12:55:15 +0530

    updated pooja's and other fro better build

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    NEXT_PUBLIC_API_URL: 'https://devine-backend-889o.onrender.com',
-    BACKEND_URL: 'https://devine-backend-889o.onrender.com',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://devine-backend-889o.onrender.com',
+    BACKEND_URL: process.env.BACKEND_URL || 'https://devine-backend-889o.onrender.com',
   },
 };
 
